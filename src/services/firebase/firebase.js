ecdafc5737c56f5f0668825b3db5424e57c25f71
luis <luistenan@gmail.com>
2023-02-01 16:56:31 -0400

    Add functions to handle profile from firebase
    Functions to add, get and uptate profile data from firestore and
    storage

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -26,3 +26,73 @@ export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
+
+// Functions to create and update profile
+export async function getUserByUid(uid) {
+  try {
+    const usersRef = collection(db, "users");
+    const docRef = doc(usersRef, uid);
+    const docSnap = await getDoc(docRef);
+    if (docSnap.exists()) {
+      const doc = docSnap.data();
+      return doc;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function existUsername(username) {
+  const users = [];
+  const docsRef = collection(db, "users");
+  const q = query(docsRef, where("username", "==", username));
+  const querySnapshot = await getDocs(q);
+
+  querySnapshot.forEach((doc) => {
+    users.push(doc.data());
+  });
+  return users.length > 0 ? users[0].uid : false;
+}
+
+export async function createUser(uid, user) {
+  try {
+    const collectionRef = collection(db, "users");
+    const docref = doc(collectionRef, uid);
+    setDoc(docref, user);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function updateUser(uid, user) {
+  try {
+    const collectionRef = collection(db, "users");
+    const docref = doc(collectionRef, uid);
+    setDoc(docref, user);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function setUserProfilePhoto(id, file) {
+  try {
+    const profilePhotoRef = ref(storage, `images/profile/${id}`);
+    const resUpload = await uploadBytes(profilePhotoRef, file);
+    return true;
+  } catch (error) {
+    console.error(error);
+    return false;
+  }
+}
+
+export async function downloadUserProfilePhoto(id) {
+  try {
+    const profilePhotoRef = ref(storage, `images/profile/${id}`);
+    const resDownload = await getDownloadURL(profilePhotoRef);
+    return resDownload;
+  } catch (error) {
+    console.error(error);
+  }
+}
