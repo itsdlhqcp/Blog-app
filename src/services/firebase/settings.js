8d7f3afe53e4e927ff726fd5c8ebaa3bc0085fc1
itsdlhqcp <dilhaqued@gmail.com>
2023-05-01 13:21:33 +0530

    first commit

--- src/services/firebase/settings.js
+++ src/services/firebase/settings.js
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  apiKey: process.env.REACT_APP_APIKEY,
-  authDomain: process.env.REACT_APP_AUTHDOMAIN,
-  projectId: process.env.REACT_APP_PROJECTID,
-  storageBucket: process.env.REACT_APP_STORAGEBUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
-  appId: process.env.REACT_APP_APPID,
+  apiKey: "AIzaSyB3LDVzgWOZYyh2xHBBSCTQS0GW8GJJt5c",
+  authDomain: "next-show-abc98.firebaseapp.com",
+  databaseURL: "https://next-show-abc98-default-rtdb.firebaseio.com",
+  projectId: "next-show-abc98",
+  storageBucket: "next-show-abc98.appspot.com",
+  messagingSenderId: "58888522440",
+  appId: "1:58888522440:web:da5dd8aeec079c45d773fd"
 };
